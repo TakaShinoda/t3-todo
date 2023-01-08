e21870ac1a2ae24b3bd99145bb74f883b4e091f0
Takahiro <tkhr_621@docomo.ne.jp>
2023-01-09 02:07:14 +0900

    create hook

---
+++ src/hooks/useMutateTask.ts
@@ -0,0 +1,50 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import useStore from "../store"
+import { api } from "../utils/api"
+
+
+export const useMutateTask = () => {
+  const utils = api.useContext()
+  const reset = useStore((state) => state.resetEditedTask)
+
+  const createTaskMutation = api.todo.createTask.useMutation({
+    onSuccess: (res) => {
+      // 既存のtodoのキャッシュを取得
+      const prevTodos = utils.todo.getTasks.getData()
+      if(prevTodos) {
+        utils.todo.getTasks.setData(undefined, [res, ...prevTodos])
+      }
+      reset()
+    },
+  })
+  const updateTaskMutation = api.todo.updateTask.useMutation({
+    onSuccess: (res) => {
+      const prevTodos = utils.todo.getTasks.getData()
+      if(prevTodos) {
+        utils.todo.getTasks.setData(undefined,
+          prevTodos.map((task) => (task.id === res.id ? res : task))
+        )
+      }
+      reset()
+    }
+  })
+  const deleteTaskMutation = api.todo.deleteTask.useMutation({
+    //第1引数: deleteTaskの戻り値
+    //第2引数: deleteTaskに渡されたinputの引数の値
+    onSuccess: (_, variables) => {
+      const prevTodos = utils.todo.getTasks.getData()
+      if(prevTodos) {
+        utils.todo.getTasks.setData(undefined,
+          prevTodos.filter((task) => task.id !== variables.taskId)
+        )
+      }
+      reset()
+    }
+  })
+
+  return { createTaskMutation, updateTaskMutation, deleteTaskMutation }
+}
