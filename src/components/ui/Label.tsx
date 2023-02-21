ea1b7d59824d48f635a7c39de3011d09c04f3c1a
Takahiro <tkhr_621@docomo.ne.jp>
2023-02-21 22:24:31 +0900

    create ui components

---
+++ src/components/ui/Label.tsx
@@ -0,0 +1,21 @@
+import type { ElementRef, ComponentPropsWithoutRef } from "react";
+import { forwardRef } from "react";
+import * as LabelPrimitive from "@radix-ui/react-label";
+import { cn } from "../../utils/cn";
+
+const Label = forwardRef<
+  ElementRef<typeof LabelPrimitive.Root>,
+  ComponentPropsWithoutRef<typeof LabelPrimitive.Root>
+>(({ className, ...props }, ref) => (
+  <LabelPrimitive.Root
+    ref={ref}
+    className={cn(
+      "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+      className
+    )}
+    {...props}
+  />
+));
+Label.displayName = LabelPrimitive.Root.displayName;
+
+export { Label };
