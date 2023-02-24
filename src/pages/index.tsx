22cbf3140ab5837d38cfb8487840e2576e0e30aa
Takahiro <tkhr_621@docomo.ne.jp>
2023-02-25 00:38:33 +0900

    chore: スタイル修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
 import { type NextPage } from "next";
-import { signOut, useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import { Auth } from "../components/Auth";
-import { Layout } from "../components/Layout";
-import { ArrowLeftOnRectangleIcon } from "@heroicons/react/24/solid";
+import { Layout } from "../components/layouts/Layout";
 import { TaskForm } from "../components/TaskForm";
 import { TaskList } from "../components/TaskList";
 import { Avatar, AvatarImage, AvatarFallback } from "../components/ui/Avatar";
@@ -27,11 +26,6 @@ const Home: NextPage = () => {
         <AvatarFallback>GitHubのプロフィール画像</AvatarFallback>
       </Avatar>
       <p className="my-5 text-2xl text-zinc-800">{session?.user?.name}</p>
-      <ArrowLeftOnRectangleIcon
-        className="mb-4 h-6 w-6 cursor-pointer text-blue-600"
-        // eslint-disable-next-line @typescript-eslint/no-misused-promises
-        onClick={() => signOut()}
-      />
       <TaskForm />
       <TaskList />
     </Layout>
--- src/pages/task/[taskId].tsx
+++ src/pages/task/[taskId].tsx
@@ -9,7 +9,7 @@ import { useRouter } from "next/router";
 import { format } from "date-fns";
 import { ArrowUturnLeftIcon } from "@heroicons/react/24/solid";
 import { api } from "../../utils/api";
-import { Layout } from "../../components/Layout";
+import { Layout } from "../../components/layouts/Layout";
 
 const SingleTaskPage: NextPage = () => {
   const router = useRouter();
@@ -25,17 +25,42 @@ const SingleTaskPage: NextPage = () => {
   }
   return (
     <Layout title="Task Detail">
-      <p className="mb-3 text-xl font-bold text-blue-600">{data?.title}</p>
-      <p>{data?.body}</p>
-      <p className="my-1 text-sm">
-        {data && format(new Date(data.createdAt), "yyyy-MM-dd HH:mm:ss")}
-      </p>
-      <p className="my-1 text-sm">
-        {data && format(new Date(data.updatedAt), "yyyy-MM-dd HH:mm:ss")}
-      </p>
-      <Link href={`/`}>
-        <ArrowUturnLeftIcon className="mt-3 h-6 w-6 cursor-pointer text-blue-600" />
-      </Link>
+      <div className="min-w-full overflow-hidden bg-white shadow sm:rounded-lg">
+        <div className="px-4 py-5 sm:px-6">
+          <h3 className="text-base font-semibold leading-6 text-gray-900">
+            {data?.title}
+          </h3>
+        </div>
+        <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
+          <dl className="sm:divide-y sm:divide-gray-200">
+            <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">Body</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                {data?.body}
+              </dd>
+            </div>
+            <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">updatedAt</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                {data &&
+                  format(new Date(data.updatedAt), "yyyy-MM-dd HH:mm:ss")}
+              </dd>
+            </div>
+            <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">createdAt</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                {data &&
+                  format(new Date(data.createdAt), "yyyy-MM-dd HH:mm:ss")}
+              </dd>
+            </div>
+            <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+              <Link href={`/`}>
+                <ArrowUturnLeftIcon className="h-5 w-5 cursor-pointer text-blue-600" />
+              </Link>
+            </div>
+          </dl>
+        </div>
+      </div>
     </Layout>
   );
 };
