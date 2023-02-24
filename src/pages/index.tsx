329580858fb8f95e823ed86ac1470358638bdadd
Takahiro <tkhr_621@docomo.ne.jp>
2023-02-24 20:23:24 +0900

    radix のコンポーネントを使うようにする

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,9 @@ import { signOut, useSession } from "next-auth/react";
 import { Auth } from "../components/Auth";
 import { Layout } from "../components/Layout";
 import { ArrowLeftOnRectangleIcon } from "@heroicons/react/24/solid";
-// import { api } from "../utils/api";
 import { TaskForm } from "../components/TaskForm";
 import { TaskList } from "../components/TaskList";
+import { Avatar, AvatarImage, AvatarFallback } from "../components/ui/Avatar";
 
 const Home: NextPage = () => {
   const { data: session } = useSession();
@@ -19,12 +19,19 @@ const Home: NextPage = () => {
   }
   return (
     <Layout title="Todo App">
+      <Avatar className="h-48 w-48 border-2 border-cyan-500">
+        <AvatarImage
+          src={session?.user?.image as string}
+          alt={`${session?.user?.name as string}のプロフィール画像`}
+        />
+        <AvatarFallback>GitHubのプロフィール画像</AvatarFallback>
+      </Avatar>
+      <p className="my-5 text-2xl text-zinc-800">{session?.user?.name}</p>
       <ArrowLeftOnRectangleIcon
-        className="h-6 w-6 cursor-pointer text-blue-600"
+        className="mb-4 h-6 w-6 cursor-pointer text-blue-600"
         // eslint-disable-next-line @typescript-eslint/no-misused-promises
         onClick={() => signOut()}
       />
-      <p className="my-3 text-xl text-blue-600">{session?.user?.name}</p>
       <TaskForm />
       <TaskList />
     </Layout>
